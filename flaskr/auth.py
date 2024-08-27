294e01eb713366bbebf44179cdc021e139026a82
Alejo Rios <alejoriosiglesias@gmail.com>
2024-08-27 11:23:47 +0100

    bug: solving a problem connection

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,91 +1,122 @@
 import functools
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 
-bp = Blueprint('auth', __name__, url_prefix='/auth') # This creates a Blueprint named 'auth'. Like the application object, the blueprint needs to know where it’s defined, so __name__ is passed as the second argument. The url_prefix will be prepended to all the URLs associated with the blueprint.
+from .db import get_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth") # This creates a Blueprint named 'auth'. Like the application object, the blueprint needs to know where it’s defined, so __name__ is passed as the second argument. The url_prefix will be prepended to all the URLs associated with the blueprint.
+
+@bp.route('/')
+def index():
+    return "Hello, World!"
+
+
+def login_required(view):
+    """View decorator that redirects anonymous users to the login page."""
+
+    @functools.wraps(view) # This decorator returns a new view function that wraps the original view it's applied to.
+    def wrapped_view(**kwargs): # It checks if a user is loaded and redirects to the login page otherwise. If is loaded the original view is called and continues normally.
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.before_app_request # register a function that runs before the view function, no matter what URL is requested.
+def load_logged_in_user(): #checks if a user id is stored in the session, gets user's data from database, storing it on g.user
+    """If a user id is stored in the session, load the user object from
+    the database into ``g.user``."""
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
 
 # Create first view: register
-@bp.route('/register', methods=('GET', 'POST')) # associates URL register with the register view function
-def register():
-    if request.method == 'POST': # if the user submitted the form, request.method will be POST. Start validating the input
-        username = request.form['username'] # request.form is a special type of dict mapping form keys and values.
-        password = request.form['password']
+@bp.route("/register", methods=("GET", "POST")) # associates URL register with the register view function
+def register(): 
+    """Register a new user.
+
+    Validates that the username is not already taken. Hashes the
+    password for security.
+    """
+    if request.method == "POST": # if the user submitted the form, request.method will be POST. Start validating the input
+        username = request.form["username"]  # request.form is a special type of dict mapping form keys and values.
+        password = request.form["password"]
         db = get_db()
         error = None
-        
+
         if not username: # Validate that username and password are not empty.
             error = "Username is required."
         elif not password:
             error = "Password is required."
-        
+
         if error is None:
             try:
                 db.execute( # SQL query with ? placeholders for any user input. Tuple of values to replace the placeholders.Database take care of escaping the values so you are not vulnerable to a SQL injections.
                     "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)) # generate_password used to securely hash the password and stored.
+                    (username, generate_password_hash(password)), # generate_password used to securely hash the password and stored.
                 )
                 db.commit() # save the previous changes.
             except db.IntegrityError: # to assure that the username doesn't exist. Otherwise, a validation error is shown.
-                error = f'User {username} is already registered.'
+                # The username was already taken, which caused the
+                # commit to fail. Show a validation error.
+                error = f"User {username} is already registered."
             else:
-                return redirect(url_for('auth.login')) # after storing the user, redirect to the login page.
-        
-        flash(error) # if validation fails, a error is shown. Flash messages can be retrieved when rendering a template.
-    
-    return render_template('/auth/register.html')
+                # Success, go to the login page.
+                return redirect(url_for("auth.login")) # after storing the user, redirect to the login page.
 
-@bp.route('/login', method=('GET', 'POST'))
+        flash(error) # if validation fails, a error is shown. Flash messages can be retrieved when rendering a template.
+
+    return render_template("auth/register.html")
+
+
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    """Log in a registered user by adding the user id to the session."""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username)
+            "SELECT * FROM user WHERE username = ?", (username,)
         ).fetchone() # fetchone() returns one row from the query. If the query returns no results, it returns None.
-        
+
         if user is None:
-            error = "Incorrect username"
-        elif not check_password_hash(user['password'], password): # hashes the submitted passwords and securly compares them.
-            error = "Incorrect password"
-        
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password): # hashes the submitted passwords and securly compares them.
+            error = "Incorrect password."
+
         if error is None:
+            # store the user id in a new session and return to the index
             session.clear()
-            session['user_id'] = user['id'] # session is a dict that stores data. If validation succeeds, user's id is stored in a new session
-            return redirect(url_for('index'))
-        
+            session["user_id"] = user["id"] # session is a dict that stores data. If validation succeeds, user's id is stored in a new session
+            return redirect(url_for("index"))
+
         flash(error)
-    
-    return render_template('/auth/login.html')
 
-@bp.before_app_request # register a function that runs before the view function, no matter what URL is requested.
-def load_logged_in_user(): #checks if a user id is stored in the session, gets user's data from database, storing it on g.user
-    user_id = session.get('user_id')
-    
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id)
-        ).fetchone()
+    return render_template("auth/login.html")
 
-@bp.route('/logout')
+
+@bp.route("/logout")
 def logout():
+    """Clear the current session, including the stored user id."""
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-def login_required(view):
-    @functools.wraps(view) # This decorator returns a new view function that wraps the original view it's applied to.
-    def wrapped_view(**kwargs): # It checks if a user is loaded and redirects to the login page otherwise. If is loaded the original view is called and continues normally.
-        if g.user is None:
-            return redirect(url_for('auth.login')) 
-        # url_for() generates the URL to a view based on a name and arguments. The name associated is also called the 'endpoint', and by default it's the same as the name of the view function. 
-        # When using a blueprint, the name of the blueprint is prepended to the name of the function, so the endpoint for the 'login' function is 'auth.login' because you added it to the 'auth' blueprint.
 
-        return view(**kwargs)
-    return wrapped_view
--- flaskr/db.py
+++ flaskr/db.py
@@ -1,36 +1,53 @@
 import sqlite3
 
 import click
-from flask import current_app, g
+from flask import current_app
+from flask import g
+
 
 def get_db():
-    if 'db' not in g: # g is a special object, unique for each request. It's used to store data that might be accesed by multiple functions during the request. Connection stored and reused.
+    """Connect to the application's configured database. The connection
+    is unique for each request and will be reused if this is called
+    again.
+    """
+    if "db" not in g: # g is a special object, unique for each request. It's used to store data that might be accesed by multiple functions during the request. Connection stored and reused.
         g.db = sqlite3.connect( # 'sqlite3.connect' establishes a connection to the file pointed at by the DATABASE config key.This file doesn't have to exist yet.
-            current_app.config['DATABASE'], # 'current_app' special object that points to the Flask app handling the request. 'get_db' will be called when the app's been created and is handling a request.
-            detect_types=sqlite3.PARSE_DECLTYPES
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
+            # 'current_app' special object that points to the Flask app handling the request. 'get_db' will be called when the app's been created and is handling a request.
         )
         g.db.row_factory = sqlite3.Row #'sqlite3.Row' tells the connection to return rows that behave like dicts. It allows accessing the columns by name.
-    
+
     return g.db
 
+
 def close_db(e=None): # checks if a connection was created by checking if 'g.db' was set. If connection exists, it is closed. 
-    db =g.pop('db', None)
-    
+    """If this request connected to the database, close the
+    connection.
+    """
+    db = g.pop("db", None)
+
     if db is not None:
         db.close()
-        
-def init_db():
-    db = get_db() #returns a database connection, used it to execute the commands read from file
-    
-    with current_app.open_resource('schema.sql') as f: # opens a file relative to 'flaskr' package
-        db.executescript(f.read().decode('utf8'))
-        
-@click.command('init-db') #defines a command line command called 'init-db' that calls the 'init-db' function and shows a success message to the user.
+
+
+def init_db():  #returns a database connection, used it to execute the commands read from file
+    """Clear existing data and create new tables."""
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f: # opens a file relative to 'flaskr' package
+        db.executescript(f.read().decode("utf8"))
+
+
+@click.command("init-db") #defines a command line command called 'init-db' that calls the 'init-db' function and shows a success message to the user.
 def init_db_command():
-    """Clear the existing data and create new tables"""
+    """Clear existing data and create new tables."""
     init_db()
-    click.echo('Initialized the database')
-    
+    click.echo("Initialized the database.")
+
+
 def init_app(app):
+    """Register database functions with the Flask app. This is called by
+    the application factory.
+    """
     app.teardown_appcontext(close_db) #tells Flask to call the function when cleaning up after returning the response. 
     app.cli.add_command(init_db_command) #adds a new command that can be called with the 'flask' command.
